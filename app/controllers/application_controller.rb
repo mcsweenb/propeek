5f3646dfc4d21c9769308cfe20a881d05758e9be
Kulpreet Singh <pundit.singh@gmail.com>
2014-07-07 04:30:35 -0500

    Crop avatar as specified by the user on signup page
    Change avatar paths in profile and reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,17 +6,6 @@ class ApplicationController < ActionController::Base
   helper_method :current_user_session, :current_user
 
   before_filter :set_body_class
-
-  # def default_url_options
-  #   if Rails.env.development?
-  #     {
-  #       host: 'dev.propeek.com',
-  #       port: 80
-  #     }
-  #   else
-  #     {}
-  #   end
-  # end
   
   private
 
